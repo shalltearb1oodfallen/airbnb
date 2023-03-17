d7dbf73242098347bac9fe26f55beca20790c1e8
Shalltear Bloodfallen <shalltearbloodfallen@vivaldi.net>
2023-03-17 19:28:33 +0100

    replace bash with sh

--- raw_data/download_airbnb.sh
+++ raw_data/download_airbnb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 wget http://data.insideairbnb.com/belgium/vlg/antwerp/2022-12-29/data/listings.csv.gz -O dec_antwerp_listing.csv.gz
 wget http://data.insideairbnb.com/belgium/vlg/antwerp/2022-12-29/visualisations/listings.csv -O dec_antwerp.csv
