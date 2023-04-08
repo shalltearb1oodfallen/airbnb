a423d920e6fff8176e5acc7757d80741fd86fc00
Shalltear Bloodfallen <shalltearbloodfallen@vivaldi.net>
2023-04-08 02:29:08 +0200

    view counts adjusted

--- dbt/dbt_project/models/views/counts.sql
+++ dbt/dbt_project/models/views/counts.sql
@@ -6,19 +6,11 @@
 }}
 
 
-select latitude
-  , longitude
-  , fab.*
-
-from {{ ref('country_data') }} as cyd
-
-inner join (select count(*) as counter
-                , place
-                , country
-                , quarter
-            from {{ ref('fact_airbnb') }}
-            group by place
-                , country
-                , quarter
-            ) as fab
-    on cyd.place = fab.place
+select count(*) as counter
+    , place
+    , country
+    , quarter
+from {{ ref('fact_airbnb') }}
+group by place
+    , country
+    , quarter
